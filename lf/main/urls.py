1105da5c386193fda5163eb5b247f8d38e6e4e2c
ReFlex6975 <dreaded@bk.ru>
2024-07-20 14:30:49 +0900

    Добавил окна и возможность перехода по ним

--- lf/main/urls.py
+++ lf/main/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index),
-    path('about', views.about),
-    path('', views.mainpage)
+    path('', views.mainpage, name='mainpage'),
+    path('statistics', views.statistics, name='statistics'),
+    path('polygons', views.polygons, name='polygons'),
+    path('scenarios', views.scenarios, name='scenarios'),
+    path('about', views.about, name='about'),
+    path('cabinet', views.cabinet, name='cabinet'),
 ]
--- lf/main/views.py
+++ lf/main/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def mainpage(request):
-    return render(request,'main/mainpage.html')
-def index(request):
-    return render(request,'main/index.html')
+    return render(request,'main/mainpage.html', {'title': 'Главная'})
+def statistics(request):
+    return render(request,'main/statistics.html', {'title': 'Статистика боёв'})
+def polygons(request):
+    return render(request,'main/polygons.html', {'title': 'Полигоны'})
+def scenarios(request):
+    return render(request,'main/scenarios.html', {'title': 'Сценарии'})
 def about(request):
-    return render(request,'main/about.html')
+    return render(request,'main/about.html', {'title': 'Контакты'})
+def cabinet(request):
+    return render(request,'main/cabinet.html', {'title': 'Личный кабинет'})
+
 
